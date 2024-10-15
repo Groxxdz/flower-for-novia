55c631d01b3c6388a40bd123587e4233d4ddb784
Groxxdz <maulanaradithyan@gmail.com>
2024-10-15 07:57:09 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'HELLO MY LOVE NOVIA'.split('')
+const text = 'I Have Somethings'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
